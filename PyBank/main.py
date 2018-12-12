c0e3efd02db1916b465b92f8027480ca4aaeb0cc
codagirl <kmozamer@gmail.com>
2018-12-11 20:14:17 -0500

    add python files

---
+++ PyBank/main.py
@@ -0,0 +1,16 @@
+import csv
+import os
+
+
+
+# create file path and save as file
+open = ("budget_data.csv","r")
+#file = 'budget_data.csv'
+#emply lists for month and revenue data
+months = []
+revenue = []
+
+#read csv and parse data into lists
+#revenue list will be list of integers
+#with open(file, 'r') as csvfile:
+ #   csvread = csv.reader(csvfile)
