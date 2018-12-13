9cb2be908fa39fa78582d6410a3b276098c48920
codagirl <kmozamer@gmail.com>
2018-12-12 23:51:59 -0500

    updates

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -2,49 +2,48 @@ import os
 import csv
 
 
-#tacotaco
-# Identifies file with poll data
-file = os.path.join('..', 'PyPoll', 'election_data.csv')
 
-#Creates dictionary to be used for candidate name and vote count.
-poll = {}
+csvpath = os.path.join('..', 'PyPoll', 'election_data.csv')
 
-#Sets variable, total votes, to zero for count.
 total_votes = 0
 
-#gets data file
-with open(file, 'r') as csvfile:
-    csvread = csv.reader(csvfile)
-
-    next(csvread, None)
-
-    #creates dictionary from file using column 3 as keys, using each name only once.
-    #counts votes for each candidate as entries
-    #keeps a total vote count by counting up 1 for each loop (# of rows w/o header)
-    for row in csvread:
-        total_votes += 1
-        if row[2] in poll.keys():
-            poll[row[2]] = poll[row[2]] + 1
-        else:
-            poll[row[2]] = 1
- 
-#create empty list for candidates and his/her vote count
 candidates = []
 num_votes = []
 
-#takes dictionary keys and values and, respectively, dumps them into the lists, 
-# candidates and num_votes
-for key, value in poll.items():
-    candidates.append(key)
-    num_votes.append(value)
+#gets data file
+with open(csvpath, 'r') as csvfile:
+    csvread = csv.reader(csvfile)
+
+    row=next(csvread, None)
+    
+   
+    #counts votes 
+    for row in csvread:
+        candidate=row[2]
+        total_votes += 1
+        if candidate in candidates:
+            index=candidates.index(candidate)
+            num_votes[index]+= 1
+        else:
+            candidates.append(candidate)
+            num_votes.append(1)
+
 
 # creates vote percent list
 vote_percent = []
-for n in num_votes:
-    vote_percent.append(round(n/total_votes*100, 1))
 
-# zips candidates, num_votes, vote_percent into tuples
-clean_data = list(zip(candidates, num_votes, vote_percent))
+percent = []
+
+for count in range(len(candidates)):
+    vote_percent=round(num_votes[count]/total_votes*100,1)
+    percent.append(vote_percent)
+    
+ vote_percent = []
+for n in num_votes:
+    vote_percent.append(round(n/total_votes*100, 1))   
+
+#zips candidates, num_votes, vote_percent into tuples
+clean_data = list(zip(candidates, range(len(candidates)), percent))
 
 #creates winner_list to put winners (even if there is a tie)
 winner_list = []
@@ -53,23 +52,23 @@ for name in clean_data:
     if max(num_votes) == name[1]:
         winner_list.append(name[0])
 
-# makes winner_list a str with the first entry
+ #makes winner_list a str with the first entry
 winner = winner_list[0]
 
 #only runs if there is a tie and puts additional winners into a string separated by commas
 if len(winner_list) > 1:
-    for w in range(1, len(winner_list)):
+   for w in range(1, len(winner_list)):
         winner = winner + ", " + winner_list[w]
 
 #prints to file
 output_file = os.path.join('..', 'PyPoll', 'OutputPoll.csv')
 
 with open(output_file, 'w') as csvfile:
-    csvfile.writelines('Election Results \n------------------------- \nTotal Votes: ' + str(total_votes) + 
-      '\n-------------------------\n')
-    for entry in clean_data:
-        csvfile.writelines(entry[0] + ": " + str(entry[2]) +'%  (' + str(entry[1]) + ')\n')
-    csvfile.writelines('------------------------- \nWinner: ' + winner + '\n-------------------------')
+    csvfile.writelines('Election Results \n------ \nTotal Votes: ' + str(total_votes) + 
+      '\n-------\n')
+    for count in range(len(candidates)):
+        csvfile.writelines(f"{candidates[count]}:{percent[count]}% ({num_votes[count]})\n")
+    csvfile.writelines('---------- \nWinner: ' + winner + '\n-------')
 
 #prints file to terminal
 with open(output_file, 'r') as readfile:
