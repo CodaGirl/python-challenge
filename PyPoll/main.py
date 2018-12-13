05219d55cacede36e58952a4ec6bb6e7c5de0ab5
codagirl <kmozamer@gmail.com>
2018-12-13 00:12:09 -0500

    Updates

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -28,37 +28,24 @@ with open(csvpath, 'r') as csvfile:
             candidates.append(candidate)
             num_votes.append(1)
 
+ 
 
-# creates vote percent list
-vote_percent = []
+# find percentage and winner
+
 
 percent = []
+maxvote=num_votes[0]
+maxindex=0
 
 for count in range(len(candidates)):
     vote_percent=round(num_votes[count]/total_votes*100,1)
     percent.append(vote_percent)
     
- vote_percent = []
-for n in num_votes:
-    vote_percent.append(round(n/total_votes*100, 1))   
-
-#zips candidates, num_votes, vote_percent into tuples
-clean_data = list(zip(candidates, range(len(candidates)), percent))
-
-#creates winner_list to put winners (even if there is a tie)
-winner_list = []
-
-for name in clean_data:
-    if max(num_votes) == name[1]:
-        winner_list.append(name[0])
-
- #makes winner_list a str with the first entry
-winner = winner_list[0]
-
-#only runs if there is a tie and puts additional winners into a string separated by commas
-if len(winner_list) > 1:
-   for w in range(1, len(winner_list)):
-        winner = winner + ", " + winner_list[w]
+    if num_votes[count]>maxvote:
+        maxvote=num_votes[count]
+        maxindex=count
+winner=candidates[maxindex]
+    
 
 #prints to file
 output_file = os.path.join('..', 'PyPoll', 'OutputPoll.csv')
@@ -68,7 +55,7 @@ with open(output_file, 'w') as csvfile:
       '\n-------\n')
     for count in range(len(candidates)):
         csvfile.writelines(f"{candidates[count]}:{percent[count]}% ({num_votes[count]})\n")
-    csvfile.writelines('---------- \nWinner: ' + winner + '\n-------')
+    csvfile.writelines('---------- \nWinner: ' + winner+ '\n-------')
 
 #prints file to terminal
 with open(output_file, 'r') as readfile:
